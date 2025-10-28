85ff248477d9d42aaa8a5239d7a3f831cfe71608
Durga-Prasad62 <majjidurgaprasad90@gmail.com>
2025-10-28 13:18:44 +0530

    firstjs

---
+++ index.js
@@ -0,0 +1,17 @@
+var name ="prasad"
+console.log(name)
+name = "durga prasad"
+console.log(name)
+var age = 22
+console.log(age)
+let name1 ="prasad"
+console.log(name1)
+ name1 ="durga"
+console.log(name1)//reassign is ok
+// let name1 ="durgaprasad" Re Declaration Not Possible
+const value=134
+console.log(value)
+value =135
+console.log(value)// Reassignment not Possible
+// const value =1234
+console.log(value)//Redeclaration is not possible
